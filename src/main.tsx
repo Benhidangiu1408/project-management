08f1184f9e30a0c6c971548a823a62cd33340eec
vck2004 <52705258+vck2004@users.noreply.github.com>
2024-12-30 12:26:27 +0700

    g

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import Login from "./pages/Login/Login";
 
 const queryClient = new QueryClient();
 
